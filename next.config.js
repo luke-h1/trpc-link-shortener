50d8114cc4515cddbb848ae0074c623e458f961f
luke-h1 <luke.howsam@yahoo.com>
2023-03-25 20:16:30 +0000

    init commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
